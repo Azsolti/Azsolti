66549457946047a63a03467d75e3c56b3f2abff9
Azsolti <89606115+Azsolti@users.noreply.github.com>
2022-10-04 12:52:56 +0200

    Update VMT BestFit Visu Offset updater_HU v1.00.py

--- VMT BestFit Visu Offset updater_HU v1.00.py
+++ VMT BestFit Visu Offset updater_HU v1.00.py
@@ -1,4 +1,3 @@
-import csv
 import xml.etree.ElementTree as ET
 from tkinter import *
 from tkinter import filedialog
@@ -10,11 +9,10 @@ import psutil
 import shutil
 import time
 
-
 root = Tk()
 root.configure(bg='light yellow')
 root.title('VMT BestFit Visu Offset updater_HU v1.00')
-label = Label(root, text='Válaszd ki az ofszetfájlt(.txt formátum)',
+label = Label(root, text='Válaszd ki az ofszetfájlt(.csv formátum)',
               font=('Arial', 13, 'bold'), bg='light yellow')
 label.pack()
 exact_time = datetime.now()
@@ -59,8 +57,8 @@ path_list_C118 = ["D:\BestFit\AuditTypes_Left\\100_C118_FD_LI.xml",
                   "D:\BestFit\AuditTypes\Type_C118_AMG\\22_C118_KV_LBR_Rechts_AMG.xml",
                   "D:\BestFit\AuditTypes_KH_Left\120_C118_AMG_KH_LI.xml",
                   "D:\BestFit\AuditTypes_KH_Right\120_C118_AMG_KH_RE.xml",
-                  "D:\BestFit\AuditTypes_MH\120_C118_AMG_MH.xml"                  
-                 ]
+                  "D:\BestFit\AuditTypes_MH\120_C118_AMG_MH.xml"
+                  ]
 
 path_list_X118 = ["D:\BestFit\AuditTypes_Left\\140_X118_FD_LI.xml",
                   "D:\BestFit\AuditTypes_Right\\140_X118_FD_RE.xml",
@@ -73,13 +71,13 @@ path_list_X118 = ["D:\BestFit\AuditTypes_Left\\140_X118_FD_LI.xml",
                   "D:\BestFit\AuditTypes_STS_vo_Left\\141_X118_STS_vo_li.xml",
                   "D:\BestFit\AuditTypes_STS_vo_Right\\141_X118_STS_vo_re.xml",
                   "D:\BestFit\AuditTypes\Type_X118\\31_X118_KV_LBR_Links.xml",
-                  "D:\BestFit\AuditTypes\Type_X118\\31_X118_KV_LBR_Rechts.xml"
-                  "D:\BestFit\AuditTypes_MH\\160_X118_AMG_MH.xml"
-                  "D:\BestFit\AuditTypes_STS_vo_Left\\161_X118_AMG_STS_vo_li.xml"
-                  "D:\BestFit\AuditTypes_STS_vo_Right\\161_X118_AMG_STS_vo_re.xml"
-                  "D:\BestFit\AuditTypes\Type_X118_AMG\\32_X118_KV_LBR_Links_AMG.xml"
-                  "D:\BestFit\AuditTypes\Type_X118_AMG\\32_X118_KV_LBR_Rechts_AMG.xml"
-                  "D:\BestFit\AuditTypes_KH_Left\160_X118_AMG_KH_LI.xml"
+                  "D:\BestFit\AuditTypes\Type_X118\\31_X118_KV_LBR_Rechts.xml",
+                  "D:\BestFit\AuditTypes_MH\\160_X118_AMG_MH.xml",
+                  "D:\BestFit\AuditTypes_STS_vo_Left\\161_X118_AMG_STS_vo_li.xml",
+                  "D:\BestFit\AuditTypes_STS_vo_Right\\161_X118_AMG_STS_vo_re.xml",
+                  "D:\BestFit\AuditTypes\Type_X118_AMG\\32_X118_KV_LBR_Links_AMG.xml",
+                  "D:\BestFit\AuditTypes\Type_X118_AMG\\32_X118_KV_LBR_Rechts_AMG.xml",
+                  "D:\BestFit\AuditTypes_KH_Left\160_X118_AMG_KH_LI.xml",
                   "D:\BestFit\AuditTypes_KH_Right\160_X118_AMG_KH_RE.xml"
                   ]
 
@@ -138,10 +136,10 @@ def create_xml_backup():
 def restore_xml_backup():
     ask_question = messagebox.askquestion('Figyelmeztetés', 'Valóban visszatöltöd a biztonsági másolatot?')
     if ask_question == 'yes':
-        backup_files = os.listdir("D:\\BestFit\\BackupBestFit\\AbgleichBackup")
+        backup_files = os.listdir("D:\\BestFit\\BackupBestFit\\AbgleichBackup\\")
 
         for file_path, backup_path in zip(backup_files, backup_pathlist):
-            shutil.copy2("D:\\BestFit\\BackupBestFit\\AbgleichBackup" + file_path, backup_path)
+            shutil.copy2("D:\\BestFit\\BackupBestFit\\AbgleichBackup\\" + file_path, backup_path)
 
         open_button_W177["state"] = "normal"
         open_button_W177['font'] = ('Arial', 29, 'bold')
@@ -156,210 +154,206 @@ def restore_xml_backup():
         open_button_X118['font'] = ('Arial', 30, 'bold')
         open_button_X118["bd"] = 10
         backup_button['state'] = 'disabled'
-        messagebox.showinfo("Sikeres helyreállítás", "Biztonsági mentés sikeresen visszatöltve. MINDEN offset az eredeti értékre állítva!")
+        messagebox.showinfo("Sikeres helyreállítás",
+                            "Biztonsági mentés sikeresen visszatöltve. MINDEN offset az eredeti értékre állítva!")
 
 
 def start_program_W177():
     while True:
-        csv_rawdata = []
-        rawdata_chars = []
-        final_list = []
-        list_tostrg = ''
         try:
-            with open(open_file(), 'r') as csv_file:
-                reader = csv.reader(csv_file)
-                for n in reader:
+            csv_rawdata = []
+            replaced_char_list = []
+            replaced_char_list_2 = []
+            final_point_value_list = []
+            filepath = filedialog.askopenfilename(title='Válaszd ki az XML fájlt', filetypes=(("csv files", "*.csv"),
+                                                                                              ("all files", "*.*")))
+
+            txt_path = filepath[:len(filepath) - 4]
+            final_path_to_txt = txt_path + ".txt"
+            csv_copy = shutil.copyfile(filepath, final_path_to_txt)
+
+            with open(csv_copy, 'r') as output_file:
+                for n in output_file:
                     csv_rawdata.append(n)
-        except TypeError:
-            break
 
-        try:
             for n in csv_rawdata:
-                for character in n:
-                    list_tostrg += character
-                    strg_replace = list_tostrg.replace(";", ".", 1)
-                    list_tostrg = ""
-                    rawdata_chars.append(strg_replace)
+                replaced = n.replace(';', '.', 1)
+                replaced_char_list.append(replaced)
 
-                # Converting unwanted characters to readable/necessary ones
-            for n in rawdata_chars:
-                list_tostrg += n
-                strg_replace2 = list_tostrg.replace(";", "=")
-                final_list.append(strg_replace2)
-                list_tostrg = ""
+            for n in replaced_char_list:
+                replaced_2 = n.replace(';', '=')
+                replaced_char_list_2.append(replaced_2)
 
-            for item in final_list:
+            for n in replaced_char_list_2:
+                replaced_3 = n.replace(',', '.')
+                final_point_value_list.append(replaced_3[:-1])
+
+            for item in final_point_value_list:
                 getindex = item.index("=")
                 name_puff = item[:getindex]
                 value_puff = item[getindex + 1:]
                 csv_origdata[name_puff] = value_puff
 
-        except Exception:
-            messagebox.showerror('Érvénytelen fájl', 'Kérlek válaszd ki a típusnak megfelelö fájlt')
-            break
+            if "W177" in str(final_path_to_txt):
+                for file_path in existing_path_W177:
+                    setmeasurement_pointc118(file_path)
+                open_button_W177["text"] = "W177 offsetek beadva"
+                open_button_W177['font'] = ('Arial', 20, 'italic')
+                open_button_W177["state"] = "disabled"
+                backup_button['state'] = 'normal'
+                break
 
-        if "W177" in str(csv_file):
-            for file_path in existing_path_W177:
-                setmeasurement_pointc118(file_path)
-            open_button_W177["text"] = "W177 offszetelve"
-            open_button_W177['font'] = ('Arial', 20, 'italic')
-            open_button_W177["state"] = "disabled"
-            backup_button['state'] = 'normal'
-
+        except:
             break
 
         else:
-            messagebox.showerror('Érvénytelen', 'Kérlek válaszd ki a típusnak megfelelö fájlt')
+            messagebox.showerror('Helytelen fájl', 'Kérlek válaszd ki a megfelelő XML fájlt!')
 
 
 def start_program_C118():
     while True:
-        csv_rawdata = []
-        rawdata_chars = []
-        final_list = []
-        list_tostrg = ''
         try:
-            with open(open_file(), 'r') as csv_file:
+            csv_rawdata = []
+            replaced_char_list = []
+            replaced_char_list_2 = []
+            final_point_value_list = []
+            filepath = filedialog.askopenfilename(title='Válaszd ki az XML fájlt', filetypes=[("csv files", "*.csv"),
+                                                                                              ("all files", "*.*")])
 
-                reader = csv.reader(csv_file)
-                for n in reader:
+            txt_path = filepath[:len(filepath) - 4]
+            final_path_to_txt = txt_path + ".txt"
+            csv_copy = shutil.copyfile(filepath, final_path_to_txt)
+
+            with open(csv_copy, 'r') as output_file:
+                for n in output_file:
                     csv_rawdata.append(n)
-        except Exception:
-            break
 
-        try:
             for n in csv_rawdata:
-                for character in n:
-                    list_tostrg += character
-                    strg_replace = list_tostrg.replace(";", ".", 1)
-                    list_tostrg = ""
-                    rawdata_chars.append(strg_replace)
+                replaced = n.replace(';', '.', 1)
+                replaced_char_list.append(replaced)
 
-                # Converting unwanted characters to readable/necessary ones
-            for n in rawdata_chars:
-                list_tostrg += n
-                strg_replace2 = list_tostrg.replace(";", "=")
-                final_list.append(strg_replace2)
-                list_tostrg = ""
+            for n in replaced_char_list:
+                replaced_2 = n.replace(';', '=')
+                replaced_char_list_2.append(replaced_2)
 
-            for item in final_list:
+            for n in replaced_char_list_2:
+                replaced_3 = n.replace(',', '.')
+                final_point_value_list.append(replaced_3[:-1])
+
+            for item in final_point_value_list:
                 getindex = item.index("=")
                 name_puff = item[:getindex]
                 value_puff = item[getindex + 1:]
                 csv_origdata[name_puff] = value_puff
 
-        except Exception:
-            messagebox.showerror('Érvénytelen fájl', 'Kérlek válaszd ki a típusnak megfelelö fájlt')
-            break
-
-        if "C118" in str(csv_file):
-            for file_path in existing_path_C118:
-                setmeasurement_pointc118(file_path)
-            open_button_C118["text"] = "C118 offszetelve"
-            open_button_C118['font'] = ('Arial', 20, 'italic')
-            open_button_C118["state"] = "disabled"
-            backup_button['state'] = 'normal'
+            if "C118" in str(final_path_to_txt):
+                for file_path in existing_path_C118:
+                    setmeasurement_pointc118(file_path)
+                open_button_C118["text"] = "C118 offsetek beadva"
+                open_button_C118['font'] = ('Arial', 20, 'italic')
+                open_button_C118["state"] = "disabled"
+                backup_button['state'] = 'normal'
+                break
+        except:
             break
         else:
-            messagebox.showerror('Érvénytelen fájl', 'Kérlek válaszd ki a típusnak megfelelö fájlt')
+            messagebox.showerror('Helytelen fájl', 'Kérlek válaszd ki a megfelelő XML fájlt!')
 
 
 def start_program_X118():
     while True:
-        csv_raw_data = []
-        raw_data_chars = []
-        final_list = []
-        list_tostrg = ''
         try:
-            with open(open_file(), 'r') as csv_file:
+            csv_rawdata = []
+            replaced_char_list = []
+            replaced_char_list_2 = []
+            final_point_value_list = []
+            filepath = filedialog.askopenfilename(title='Select the .txt file', filetypes=(("csv files", "*.csv"),
+                                                                                           ("all files", "*.*")))
 
-                reader = csv.reader(csv_file)
-                for n in reader:
-                    csv_raw_data.append(n)
-        except Exception:
-            break
+            txt_path = filepath[:len(filepath) - 4]
+            final_path_to_txt = txt_path + ".txt"
+            csv_copy = shutil.copyfile(filepath, final_path_to_txt)
 
-        try:
-            for n in csv_raw_data:
-                for character in n:
-                    list_tostrg += character
-                    strg_replace = list_tostrg.replace(";", ".", 1)
-                    list_tostrg = ""
-                    raw_data_chars.append(strg_replace)
+            with open(csv_copy, 'r') as output_file:
+                for n in output_file:
+                    csv_rawdata.append(n)
 
-            for n in raw_data_chars:
-                list_tostrg += n
-                strg_replace2 = list_tostrg.replace(";", "=")
-                final_list.append(strg_replace2)
-                list_tostrg = ""
+            for n in csv_rawdata:
+                replaced = n.replace(';', '.', 1)
+                replaced_char_list.append(replaced)
 
-            for item in final_list:
+            for n in replaced_char_list:
+                replaced_2 = n.replace(';', '=')
+                replaced_char_list_2.append(replaced_2)
+
+            for n in replaced_char_list_2:
+                replaced_3 = n.replace(',', '.')
+                final_point_value_list.append(replaced_3[:-1])
+
+            for item in final_point_value_list:
                 getindex = item.index("=")
                 name_puff = item[:getindex]
                 value_puff = item[getindex + 1:]
                 csv_origdata[name_puff] = value_puff
 
-        except Exception:
-            messagebox.showerror('Érvénytelen fájl', 'Kérlek válaszd ki a típusnak megfelelö fájlt')
+            if "X118" in str(final_path_to_txt):
+                for file_path in existing_path_X118:
+                    setmeasurement_pointc118(file_path)
+                open_button_X118["text"] = "X118 offsetek beadva"
+                open_button_X118['font'] = ('Arial', 20, 'italic')
+                open_button_X118["state"] = "disabled"
+                backup_button['state'] = 'normal'
+                break
+        except:
             break
-
-        if "X118" in str(csv_file):
-            for file_path in existing_path_X118:
-                setmeasurement_pointc118(file_path)
-            open_button_X118["text"] = "X118 offsetelve"
-            open_button_X118['font'] = ('Arial', 20, 'italic')
-            open_button_X118["state"] = "disabled"
-            backup_button['state'] = 'normal'
-            break
-
         else:
-            messagebox.showerror('Érvénytelen fájl', 'Kérlek válaszd ki a típusnak megfelelö fájlt')
+            messagebox.showerror('Helytelen fájl', 'Kérlek válaszd ki a megfelelő XML fájlt!')
 
 
 def ask_include():
     ask_includexml = messagebox.askquestion("Figyelmeztetés", "Érvényesítetted (include) az XML fájlt ?")
     if ask_includexml == "yes":
-       return "yes" 
+        return "yes"
     else:
-       return "no"
-             
- 
+        return "no"
+
+
 def kill_visu_start_bf():
     visu = "BestFitVisu.exe"
-    
+    best_fit = "BestFit.exe"
     for proc in psutil.process_iter():
         if proc.name() == visu:
             proc.kill()
-            
-    while True:    
-        for proc in psutil.process_iter():
-            if proc.name() == visu:
-                messagebox.showerror("Figyelmeztetés", "Kérlek várj amíg a BestFitVisu.exe bezár") 
-                time.sleep(3)
-            else:
-                os.startfile("D://BestFit//BestFit.exe")
-                break
-              
+            break
+        else:
+            break
+
+    os.startfile("D://BestFit//BestFit.exe")
+    time.sleep(3)
+    for proc in psutil.process_iter():
+        if proc.name() == best_fit:
+            break
+
 
 def exit_program():
     if ask_include() == "yes":
-       kill_visu_start_bf()
-       root.destroy()
-       exit()
+        kill_visu_start_bf()
+        root.destroy()
+        exit()
     else:
-        messagebox.showinfo("Figyelmeztetés", "Kérlek érvényesítsd az XML fájt.")    
-        
+        messagebox.showinfo("Figyelmeztetés", "Kérlek érvényesítsd az XML fájt.")
+
 
 def open_file():
     filepath = filedialog.askopenfilename(title='Válaszd ki a  .txt fájlt', filetypes=(("txt files", "*.txt"),
-                                                                                   ("all files", "*.*")))
+                                                                                       ("all files", "*.*")))
     if filepath:
         return str(filepath)
 
 
 def value_popup(pname, pvalue):
     return messagebox.showinfo('Érték magas', f'FIGYELEM! Több mint 1.00mm offset: {pname}:{pvalue},'
-                                                f' kérlek ellenörizd, hogy helyes-e')
+                                              f' kérlek ellenörizd, hogy helyes-e')
 
 
 def setmeasurement_pointc118(document):
@@ -404,14 +398,14 @@ def setmeasurement_pointc118(document):
             result = float(get_pointValue) - float(element.get('value'))
             if abs(result) > 1:
                 value_popup(final_keylist[0], result)
-                
+
             with open(currtext, 'a+') as logfile:
-                logfile.write(f'\nOffset {final_keylist[0]} updated from {get_pointValue} to {element.get("value")}'
-                              f' in:\n {document}\n')
-                
+                logfile.write(f'\nOffset {final_keylist[0]} felülírva {get_pointValue}-ról {element.get("value")}-ra'
+                              f' itt:\n {document}\n')
+
             with open(final_logfilevar, 'a+') as logfile_2:
-                logfile_2.write(f'\nOffset {final_keylist[0]} updated from {get_pointValue} to {element.get("value")}'
-                                f'in:\n {document} \n')
+                logfile_2.write(f'\nOffset {final_keylist[0]} felülírva {get_pointValue}-ról {element.get("value")}-ra'
+                                f'itt: \n {document} \n')
 
             final_strlist.pop(0)
             final_keylist.pop(0)
@@ -420,11 +414,12 @@ def setmeasurement_pointc118(document):
         read = log.read()
         log.truncate(0)
 
-    messagebox.showinfo('Log notification', f' {read} ')
+    messagebox.showinfo('Log fájl felülírva', f' {read} ')
     pointnames_got.clear()
     values_got.clear()
     xml_origdata.clear()
     tree.write(document)
+    os.remove(currtext)
 
 
 def on_enter_W177(e):
@@ -475,7 +470,6 @@ check_fileexists_W177()
 check_fileexists_C118()
 check_fileexists_X118()
 
-
 frame1 = Frame(root, bg='black')
 frame1.pack()
 open_button_W177 = Button(frame1, text='W117 fájl megnyitás', font=('Arial', 29, 'bold'), bd='10',
@@ -500,7 +494,7 @@ open_button_X118.pack(padx=2, pady=2)
 open_button_X118.bind("<Enter>", on_enter_X118)
 open_button_X118.bind("<Leave>", on_leave_X118)
 create_xml_backup()
-backup_button = Button(root, text='Bizt.mentés betöltés', font=('Arial', 15), command=restore_xml_backup,
+backup_button = Button(root, text='Biztonsági mentés visszatöltése', font=('Arial', 15), command=restore_xml_backup,
                        state='disabled')
 backup_button.pack()
 backup_button.bind("<Enter>", on_enter_backup)
@@ -516,6 +510,5 @@ watermark = Label(root, text='<<<Created by ZMESTER>>>',
 watermark.pack(anchor='s')
 watermark2 = Label(root, text='371_Kecskemet_Rohbau_Einführung', font=('Arial', 10), bg='light yellow')
 watermark2.pack(anchor='s')
-
 
 root.mainloop()
